4e7c6147df28e0bb73a64caa599698e1577c5626
brandonrq506 <darkrahd@gmail.com>
2021-11-22 01:27:05 -0600

    Pagina Partido: Opcion de busqueda

---
+++ public/javascripts/partido.js
@@ -0,0 +1,12 @@
+//Funcionalidad para ejecutar el btn de buscar
+const btnBuscar = document.querySelector('button');
+const inputBox = document.querySelector('input');
+
+btnBuscar.addEventListener('click', () => {
+    let upperLimit = window.location.href.lastIndexOf('/');
+    let rootLink = window.location.href.substring(0, upperLimit);
+    console.log(rootLink);
+    let path = '/' + inputBox.value;
+    console.log(path);
+    window.location.href = rootLink + path;
+})
