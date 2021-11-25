5f413dc0df32ab8c54c035d19180808f91959685
brandonrq506 <darkrahd@gmail.com>
2021-11-24 21:45:58 -0600

    Created all methods to contact DB

--- routes/partido.js
+++ routes/partido.js
@@ -14,10 +14,16 @@ router.get('/', function (req, res, next) {
 
 //  localhost:3000/partido/variable/
 router.get('/:partido', function (req, res, next) {
+    let { partido } = req.params
+    getMiembros(1);
     res.render('partido', { partido });
 });
 
 module.exports = router;
+
+/*================  LLAMADAS A BASE DE DATOS ================= */
+
+
 
 //Debemos intentar revisar si podemos crear un folder especifico para llamadas a DB
 //Tipo la abstraccion que hicimos con la base de datos.
@@ -45,6 +51,95 @@ function getParties() {
         request.on('doneProc', function (rowCount, more, returnStatus, rows) {
             resolve(partidos);
         });
+        connection.callProcedure(request);
+    });
+}
+
+
+//llamamos un partido por medio de las siglas en el URL.
+function getPartido(siglas) {
+    const partido = [];
+    return new Promise((resolve, reject) => {
+        let storedProcedure = '[dbo].[spGetPartido_PorSiglas]';
+        const request = new Request(storedProcedure, function (err, rowCount) {
+            if (err) {
+                return reject(err);
+            } else {
+                resolve(partido);
+            }
+        });
+
+        request.on('row', function (fila) {
+            for (const columna of fila) {
+                partido[columna.metadata.colName] = columna.value;
+            }
+        });
+
+        request.on('doneProc', function (rowCount, more, returnStatus, rows) {
+            console.log(partido);
+            resolve(partido);
+        });
+        request.addParameter('siglas', TYPES.VarChar, siglas);
+        connection.callProcedure(request);
+    });
+}
+
+//Llamamos todas las propuestas de un partido en específico
+function getPropuestas(partidoId) {
+    const listaPropuestas = [];
+    return new Promise((resolve, reject) => {
+        let storedProcedure = '[dbo].[spGetPropuestas_PorPartido]';
+        const request = new Request(storedProcedure, function (err, rowCount) {
+            if (err) {
+                return reject(err);
+            } else {
+                resolve(listaPropuestas);
+            }
+        });
+
+        request.on('row', function (fila) {
+            let propuesta = {};
+            for (const columna of fila) {
+                propuesta[columna.metadata.colName] = columna.value;
+            }
+            listaPropuestas.push(propuesta);
+        });
+
+        request.on('doneProc', function (rowCount, more, returnStatus, rows) {
+            console.log(listaPropuestas);
+            resolve(listaPropuestas);
+        });
+        request.addParameter('partidoId', TYPES.Int, partidoId);
+        connection.callProcedure(request);
+    });
+}
+
+//Llamamos todos los miembros de un partido en específico
+function getMiembros(partidoId) {
+    const miembros = [];
+    return new Promise((resolve, reject) => {
+        let storedProcedure = '[dbo].[spGetMiembros_PorPartido]';
+        const request = new Request(storedProcedure, function (err, rowCount) {
+            if (err) {
+                return reject(err);
+            } else {
+                resolve(miembros);
+            }
+        });
+
+        request.on('row', function (fila) {
+            let miembro = {};
+            for (const columna of fila) {
+                miembro[columna.metadata.colName] = columna.value;
+            }
+            miembros.push(miembro);
+        });
+
+        request.on('doneProc', function (rowCount, more, returnStatus, rows) {
+            console.log(miembros);
+            resolve(miembros);
+        });
+        request.addParameter('partidoId', TYPES.Int, partidoId);
         connection.callProcedure(request);
     });
 }
