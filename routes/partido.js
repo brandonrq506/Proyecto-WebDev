0f15547b7e7bae0a0f093088f8e3f2601fbba056
brandonrq506 <darkrahd@gmail.com>
2021-11-24 18:12:44 -0600

    Party Homepage working fully

--- routes/partido.js
+++ routes/partido.js
@@ -6,11 +6,10 @@ const { TYPES } = require('tedious');
 
 //  localhost:3000/partido/
 router.get('/', function (req, res, next) {
-    let listaPartidos = getAllParties();
-    listaPartidos.then(partidos => {
+    let listaJSON = getParties();
+    listaJSON.then(partidos => {
         res.render('partidoHomePage', { partidos });
     });
-    //Debemos hacer algo en caso de que la llamada falle, al menos con testing purposes
 });
 
 //  localhost:3000/partido/variable
@@ -22,30 +21,29 @@ module.exports = router;
 
 //Debemos intentar revisar si podemos crear un folder especifico para llamadas a DB
 //Tipo la abstraccion que hicimos con la base de datos.
-function getAllParties() {
-    const allRows = [];
+function getParties() {
+    const partidos = [];
     return new Promise((resolve, reject) => {
-        let storedProcedure = '[dbo].[Estudiante_GetAll]';
+        let storedProcedure = '[dbo].[spGetPartidos]';
         const request = new Request(storedProcedure, function (err, rowCount) {
             if (err) {
                 return reject(err);
             } else {
-                resolve(allRows);
+                resolve(partidos);
             }
         });
 
         request.on('row', function (columns) {
 
-            let rowObject = {};
+            let partido = {};
             for (const column of columns) {
-                rowObject[column.metadata.colName] = column.value;
+                partido[column.metadata.colName] = column.value;
             }
-            allRows.push(rowObject);
+            partidos.push(partido);
         });
 
         request.on('doneProc', function (rowCount, more, returnStatus, rows) {
-            resolve(allRows);
-            console.log(allRows);
+            resolve(partidos);
         });
         connection.callProcedure(request);
     });
