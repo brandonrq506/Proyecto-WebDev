db36ce1c7d192a0bfa7e2149a109494ae2267c8f
Manuel Chavarria <85698560+mchavarriap628@users.noreply.github.com>
2021-11-25 22:11:25 -0600

    Login by Manuel

--- routes/register.js
+++ routes/register.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 const { connection } = require('../db');
 
+var express = require('express');
+var router = express.Router();
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index');
+    res.render('registerT');
 });
 
 module.exports = router;
