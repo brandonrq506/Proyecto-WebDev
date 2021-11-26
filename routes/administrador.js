df72812fd0eda042a22b1f08939dc1cd4c20400e
brandonrq506 <darkrahd@gmail.com>
2021-11-26 09:42:19 -0600

    Added Admin folder and EJS

--- routes/administrador.js
+++ routes/administrador.js
@@ -5,16 +5,16 @@ let Request = require('tedious').Request;
 const { TYPES } = require('tedious');
 
 router.get('/', function (req, res, next) {
-    res.send('Admin Home page');
+    res.render('Admin/homepage');
 });
 
 router.get('/partido', function (req, res, next) {
-    res.send('Admin PartidoHome page');
+    res.render('Admin/partidoHome');
 });
 
 router.get('/partido/:partido', function (req, res, next) {
     let { partido } = req.params;
-    res.send(`Admin partido: ${partido} page`);
+    res.render('Admin/partido');
 });
 
 
