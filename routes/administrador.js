a67abc3db5e086ba323d2c56192d1498b4212dd2
brandonrq506 <darkrahd@gmail.com>
2021-11-26 10:21:49 -0600

    DB calls abstracted

--- routes/administrador.js
+++ routes/administrador.js
@@ -4,18 +4,26 @@ const { connection } = require('../db');
 let Request = require('tedious').Request;
 const { TYPES } = require('tedious');
 
+const { getListaPartidos } = require('../DataBase/getPartidos.js')
+
 router.get('/', function (req, res, next) {
     res.render('Admin/homepage');
 });
 
 router.get('/partido', function (req, res, next) {
-    res.render('Admin/partidoHome');
+    getListaPartidos()
+        .then(partidos => {
+            console.log(partidos)
+            res.render('Admin/partidoHome', partidos);
+        })
+        .catch(err => {
+            res.send('There was an error with this call');
+        })
 });
 
 router.get('/partido/:partido', function (req, res, next) {
     let { partido } = req.params;
     res.render('Admin/partido');
 });
-
 
 module.exports = router;
