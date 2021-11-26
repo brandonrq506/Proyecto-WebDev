4f32470677169a3c06aab7829927d9d810872ee3
brandonrq506 <darkrahd@gmail.com>
2021-11-26 11:16:08 -0600

    Create Admin Page to Remove/Edit partidos

--- routes/administrador.js
+++ routes/administrador.js
@@ -4,22 +4,23 @@ var router = express.Router();
 //Estas son las llamadas a la base de datos que estamos utilizando en estas paginas de Administrador
 const { getListaPartidos } = require('../DataBase/getPartidos.js')
 
-
+//  localhost:3000/ad/
 router.get('/', function (req, res, next) {
     res.render('Admin/homepage');
 });
 
+//  localhost:3000/ad/partido/
 router.get('/partido', function (req, res, next) {
     getListaPartidos()
         .then(partidos => {
-            console.log(partidos)
-            res.render('Admin/partidoHome', partidos);
+            res.render('Admin/partidoHome', { partidos });
         })
         .catch(err => {
             res.send('There was an error with this call');
         })
 });
 
+//  localhost:3000/ad/partido/variable
 router.get('/partido/:partido', function (req, res, next) {
     let { partido } = req.params;
     res.render('Admin/partido');
