13537059074bb578059a225c6e85f4736e0cf931
brandonrq506 <darkrahd@gmail.com>
2021-12-02 17:55:52 -0600

    Organization

--- routes/administrador.js
+++ routes/administrador.js
@@ -7,33 +7,35 @@ const { deletePartido } = require('../DataBase/deletePartido.js')
 
 //  localhost:3000/ad/
 router.get('/', function (req, res, next) {
-    res.render('Admin/homepage');
+    res.render('Admin/index');
 });
 
 //  localhost:3000/ad/partido/
 router.get('/partido', function (req, res, next) {
     getListaPartidos()
         .then(partidos => {
-            res.render('Admin/partidoHome', { partidos });
+            res.render('Admin/partidos', { partidos });
         })
         .catch(err => {
             res.send('There was an error with this call');
         })
 });
 
+// ======= NOT IMPLEMENTED YET ==========================
 //  localhost:3000/ad/partido/variable
 router.get('/partido/:partido', function (req, res, next) {
     let { partido } = req.params;
     res.render('Admin/partido');
 });
 
+// localhost:3000/ad/partido/variable
 router.delete('/partido/:partido', function (req, res) {
     let { partido } = req.params;
     deletePartido(partido)
-    .then(result => {
-        res.redirect('/ad/partido');
-    })
-    
+        .then(result => {
+            res.redirect('/ad/partido');
+        })
+
 });
 
 
