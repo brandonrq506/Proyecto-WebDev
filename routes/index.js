02a21f9c9f77613e08266cef274c2c4f188b1791
brandonrq506 <darkrahd@gmail.com>
2021-12-02 21:01:43 -0600

    Log in completed

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,14 @@ router.get('/', function (req, res, next) {
   res.render('Estudiante/index');
 });
 
-router.get('/home', function (req, res) {
-  res.render('Estudiante/home')
-});
-
+//Not writting router.get('/home') eliminates the possibility of errors.
+//That page can only be loaded with personalized data.
 router.post('/home', function (req, res) {
   let { correo, contra } = req.body;
   login(correo, contra)
     .then(estudiante => {
       if (estudiante.estudianteId > 0) {
-        res.redirect('/home');
+        res.render('Estudiante/home', { estudiante });
       }
     })
 });
